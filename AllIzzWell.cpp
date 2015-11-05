859f8dd13d20ae772878aa137f88fc8cf018db5e
Akash <akash.bunde@gmail.com>
2015-11-06 00:47:35 +0530

    updating directory

--- AllIzzWell.cpp
+++ AllIzzWell.cpp
@@ -1,24 +1,20 @@
-/*
-PROBLEM SOLVING AND PROGRAMING
-ASSIGNMENT 
-NAME:- AYUSH SHARMA
-ROLL:- D-27
-*/
-
-
 #include<iostream>
 #include<string.h>
-
+#define MAX 100
 using namespace std;
 
 string s = "ALLIZZWELL";
 int s_pos = 0;
-char **mat;
-int spot_r, spot_c;
+char mat[MAX][MAX]={'\0'};
+int ROW=0,COLUMN=0;
 
 bool isFound(int r, int c, int pos)
 {
-	if(r >= 0 && c >= 0 && mat[r][c] == s[pos]) return true;
+	if((r >= 0 && c >= 0) && (r <= ROW && c <= COLUMN) && mat[r][c] == s[pos]) 
+	{
+	
+		return true;
+	}
 	
 	else return false;
 }
@@ -27,10 +23,17 @@ bool backTrack(int r, int c, int s_pos)
 {
 	int i=0;
 	
+	if(s_pos>=11)
+	{
+		return false;
+	}
+	
 	if(isFound(r, c, s_pos)==true)
 	{
-		if(s_pos == s.length()) return true;				//check final state
-		
+		mat[r][c]='0';
+	
+		if(s_pos+1 == s.length()) return true;				//check final state
+	
 		if(backTrack(r,c+1,s_pos+1) == true)			//east
 		{
 			return true;
@@ -59,9 +62,15 @@ bool backTrack(int r, int c, int s_pos)
 		{
 			return true;
 		}
+		
 		if(backTrack(r-1,c+1,s_pos+1) == true)		//north-east
 		{
 			return true;
+		}
+		else
+		{
+			mat[r][c]=s[s_pos];
+			return false;
 		}
 	}
 	
@@ -74,53 +83,54 @@ bool backTrack(int r, int c, int s_pos)
 
 int main()
 {
-	int t,r,c;
+	int t;
 	int j, k;
 	cin>>t;
 	
 	for(int i=0; i<t; i++)
 	{
-		cin>>r;
-		cin>>c;
+		cin>>ROW;
+		cin>>COLUMN;
 		
-		mat = new char *[r];							//memory allocated to rows
-		
-		for(j=0; j<r; j++)	mat[j] = new char[c];	//memory allocated to columns
-		
-		for(j=0; j<r; j++)
+		for(j=0; j<ROW; j++)
 		{
-			char str[c+1]={'\0'};
-			cin>>str;
+		
+			for(k=0; k<COLUMN; k++)
+				cin>>mat[j][k];
 			
-			cout<<"\n"<<r<<"\n"<<c;
-			
-			for(k=0; k<c; k++)
-			{
-				cout<<"\nYO IN FOR LOOP\n"<<str[k];
-				mat[r][k]=str[k];
-				cout<<"\nYO ELEMENT INSERTED";
-			}
 			
 		}
-
+		
 		
 		j = 0;
 		k = 0;
+		bool back = false;
 		
-		while(j<r && k<c && backTrack == false)		// finding start of the sentence
+		while(j<ROW && k<COLUMN && back == false)		// finding start of the sentence
 		{
 			if(mat[j][k]==s[0])
 			{
-				backTrack(r, c, 0);
+				back = backTrack(j, k, 0);
 			}
-			j++;
 			k++;
+			if(k==COLUMN)
+			{
+				k=0;
+				j++;
+			}	
 		}
 		
-		for(i = 0 ; i < r ; i++ )
-		delete [] mat[i] ;
-		delete [] mat ;
-		//if(count == 10) cout << "\nYES";
-		//count = 0;
+		if(back == true) cout << "\nYES";
+		else cout << "\nNO";
+		
+		for(j=0; j<ROW; j++)
+		{
+		
+			for(k=0; k<COLUMN; k++)
+				mat[j][k]='\0';
+			
+			
+		}
+		
 	}
 }
--- Least_Common_Ancestor.c
+++ Least_Common_Ancestor.c
@@ -3,9 +3,25 @@
 
 char * array=NULL;
 
-void add(char c)
+void add(char c, int count)
 {
+	printf("Error in function\n");
+	if(array[count]=='0')
+	{
+		printf("Error in condition\n");
+		array[count]=c;
+		return;
+	}
 	
+	else if(c>array[count])
+	{
+		add(c, 2*count+1);
+	}
+	
+	else if(c<array[count])
+	{
+		add(c, 2*count);
+	}
 }
 
 int main()
@@ -17,9 +33,16 @@ int main()
 	
 	array=malloc((length+1)*sizeof(char));
 	
+	for(i=0; i<=length; i++)
+	{
+		array[i]='0';
+	}
+	
 	for(i=1; i<=length; i++)
 	{
+		printf("\ninside loop\n");
 		scanf(" %c",element);
-		add(element);
+		printf("\nelement read\n");
+		add(element, 1);
 	}
 }
